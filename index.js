d30bc3332847543e523b6e7fc133cc3b9d491867
connorwiniarczyk <connorwiniarczyk@gmail.com>
2018-05-03 03:26:05 -0400

    made website work

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const http = require("http").Server(app);
 
 const path = require("path");
 
-app.get("/", function(req, res){
-	res.sendFile( path.join(__dirname, "index.html") );
+app.get("/download", function(req, res){
+	res.sendFile( path.join(__dirname, "/public/index.html") );
 });
 
-app.get("/mac", function(req, res){
+app.get("/download/mac", function(req, res){
 	res.download(__dirname + "/downloads/ERS.zip");
 });
 
-app.get("/linux", function(req, res){
+app.get("/download/linux", function(req, res){
 	res.download(__dirname + "/downloads/ERS-linux.zip");
 });
 
